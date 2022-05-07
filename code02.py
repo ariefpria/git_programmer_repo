67fe2992e5e72eedeffb8850b75cdc86b14fccbf
Arief PW <arief.pw@gmail.com>
2022-05-08 06:51:48 +0700

    done programmer01 on code02.py

--- code02.py
+++ code02.py
@@ -1,10 +1,13 @@
 import Pymysql
+import psycopg2 as pg
+from config import config
 
 def connect(filename, section):
     conn = None
 
     try:
         params = config(filename, section)
+        print('connecting to {} database ...'.format(section))
 
     except(Exception) as error:
         print(error)
