d8acefecd63f30b4f3cc281d99acecb1b0707a58
Arief PW <arief.pw@gmail.com>
2022-05-08 09:31:27 +0700

    done code04 from p01

--- code04.py
+++ code04.py
@@ -18,3 +18,7 @@ def connect(filename, section):
     finally:
         print("finally")
 
+def setIC_Data():
+    stmt = "select * from redcap_data where project_id = 16"
+
+    conn, cur = connect('database', 'mysql')
