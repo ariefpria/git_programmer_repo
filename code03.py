24f68adba3d679407c03e0429af8b7664e5b5f6e
Arief PW <arief.pw@gmail.com>
2022-05-08 07:59:52 +0700

    done template code03

---
+++ code03.py
@@ -0,0 +1,12 @@
+import pymysql
+
+def connect(filename, section):
+    conn = None
+
+    try:
+        params = config(filename, section)
+
+    except(Exception) as error:
+        print(error)
+    finally:
+        print("finaal')
